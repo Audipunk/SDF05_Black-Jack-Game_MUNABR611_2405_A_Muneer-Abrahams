07735735ca6422a4c4357724041c2f9dd974a562
Muneer Abrahams <audipunkwp@gmail.comgit checkout patch 1git config --global user.email audipunkwp@gmail.com>
2024-06-13 18:30:20 +0200

    update

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 let firstCard = 10
-let secondCard = 7
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+console. log(messageEl)
 
 function startGame() {
 
  if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
+    console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
+    console.log("You've got Blackjack!")
     hasBlackJack = true
 } else {
-    console.log("You're out of the game! 😭")
+    console.log("You're out of the game!")
     isAlive = false
 }
 
-console.log(message)
+messageEl.textContent = message
 }
