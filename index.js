481da4b4a08af9319cc7c11e8eb6d61640c9c089
Muneer Abrahams <audipunkwp@gmail.comgit checkout patch 1git config --global user.email audipunkwp@gmail.com>
2024-06-13 19:13:33 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 let firstCard = 10
 let secondCard = 11
+let cards = [firstCard, secondCard] 
+
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
-console. log(messageEl)
+let sunEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    renderGame()
+}
 
+function renderGame() {
+
+    cardsEl.textContent = "Cards: " +  cards[0] + " " + cards[1]
+    
+    sumEl.textContent = "Sum: " + sum
  if (sum <= 20) {
     console.log("Do you want to draw a new card?")
 } else if (sum === 21) {
@@ -20,4 +30,13 @@ function startGame() {
 }
 
 messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+let card = 7
+
+sum += card
+
+renderGame()
 }
