243d92069a1b3880bf158dfd095da773398a68a0
Muneer Abrahams <audipunkwp@gmail.comgit checkout patch 1git config --global user.email audipunkwp@gmail.com>
2024-06-13 22:49:17 +0200

    update

--- index.js
+++ index.js
@@ -43,16 +43,17 @@ function renderGame() {
     }
     sumEl.textContent = "Sum: " + sum
  if (sum <= 20) {
-    message = "Do you want to draw a new card?")
-} else if (sum === 21) {
-    message = "You've got Blackjack!")
+    message = "Do you want to draw a new card?"
+}else if (sum === 21) {
+    message = "You've got Blackjack!"
     hasBlackJack = true
 } else {
-    message = "You're out of the game!")
+    message = "You're out of the game!"
     isAlive = false
 }
 
 messageEl.textContent = message
+
 
 }
 
